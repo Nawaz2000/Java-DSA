5e787ef7b8a0493b3d205847ee04c03ea0fbdc88
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-01-08 11:28:59 +0530

    is palindrome added

---
+++ Recursion/01-If a string is palindrome/src/IsPalindrome.java
@@ -0,0 +1,24 @@
+
+public class IsPalindrome {
+
+	public static boolean isPalindrome(String str, int start, int end) {
+		if (start == end || start > end)
+			return true;
+		
+		if (str.charAt(start) == str.charAt(end))
+			return isPalindrome(str, start+1, end-1);
+		else
+			return false;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		String str = "madam";
+		int start = 0;
+		int end = str.length()-1;
+		System.out.println(isPalindrome(str, start, end));
+
+	}
+	
+}
