c1912c61b2155b7d690b5e6f9ae2297262632bc1
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-01-08 22:51:16 +0530

    Josephus Problem added

---
+++ Recursion/08-Permutations of a string/src/Permutations.java
@@ -0,0 +1,33 @@
+
+public class Permutations {
+	
+	public static String swap(String str, int i, int j) {
+		char[] arr = str.toCharArray();
+		char x = arr[i];
+		arr[i] = arr[j];
+		arr[j] = x;
+		return new String(arr);
+	}
+
+	public static void findPermutations(String str, int start, int end) {
+		if (start>=end)
+		{
+			System.out.println(str);
+			return;
+		}
+		for (int i = start; i<end; i++) {
+			str = swap(str,start,i);
+			findPermutations(str, start+1, end);
+            str = swap(str,start,i);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "ABC";
+		findPermutations(str, 0, str.length());
+
+	}
+
+}
