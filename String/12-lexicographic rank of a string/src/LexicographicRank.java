ce34bb61dadc9b121b27d2cb0946fbe2253c0d78
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-01-17 19:56:04 +0530

    lexicographic rank of a string added

--- String/12-lexicographic rank of a string/src/LexicographicRank.java
+++ String/12-lexicographic rank of a string/src/LexicographicRank.java
@@ -1,5 +1,13 @@
 import java.util.Arrays;
 
+/*
+ * Given a string, find its rank among all its permutations sorted lexicographically. 
+ * For example, rank of “abc” is 1, rank of “acb” is 2, and rank of “cba” is 6. 
+ * 
+ * Time complexity: O(n)
+ * Auxiliary space: O(1)
+ */
+
 public class LexicographicRank {
 
 	public static int rank(String str) {
@@ -7,35 +15,36 @@ public class LexicographicRank {
 		int res = 0;
 		
 		int[] count = new int[256];
-		int[] cumulativeCount = new int[256];
+				
+		int n = str.length();
+		int mul = fact(n);
 		
-		for (int i=0;i<str.length(); i++)
+		for (int i=0; i<n; i++)
 			count[str.charAt(i)]++;
 		
-		int sum=0;
-		// stores the number of elements smaller than current in cumulativeCount array
-		for (int i=0; i<256; i++) {
-			if(count[i] != 0) {
-				cumulativeCount[i] = sum;
-				sum++;
-			}
-		}
-		
 //		System.out.println(Arrays.toString(count));
-//		System.out.println(Arrays.toString(cumulativeCount));
 		
-		for (int i=0; i<str.length(); i++) {
-			res = res + cumulativeCount[str.charAt(i)]*fact(str.length()-1-i);
+		for (int i=1; i<256; i++)
+			count[i] += count[i-1];
+		
+//		System.out.println(Arrays.toString(count)+"\n");
+		
+		for (int i=0; i<n-1; i++) {
+			mul = mul/(n-i);
+			res = res + count[str.charAt(i)-1]*mul;
+			System.out.println("res " + i + " = " + res + ", char = "+str.charAt(i));
+			
+			for (int j=str.charAt(i); j<256; j++)
+				count[j]--;
+//			System.out.println(Arrays.toString(count));
 		}
 		
-		return res;
+		return res+1;
 	}
 	
 	public static int fact(int num) {
 		int res = num;
-		System.out.println("Num initial" + num);
 		while(true) {
-			System.out.println(num);
 			res = res*(--num);
 			if (num<=1)
 				break;
@@ -47,7 +56,7 @@ public class LexicographicRank {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = "CBA";
+		String str = "STRING";
 		System.out.println("Rank of " + str + " is: " + rank(str));
 
 	}
