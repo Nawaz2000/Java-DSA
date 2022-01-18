0a5035a1c56e27c66b7c30322d47529a048c2c8b
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-01-18 19:28:07 +0530

    longest proper prefix array added

---
+++ String/13-longest proper prefix array/src/LPS.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+/*
+ * Given a string s, find the length of the longest prefix, which is also a suffix. 
+ * The prefix and suffix should not overlap.
+ * 
+ * Ex- 
+ * ip = "abca"
+ * 
+ * here,
+ * proper prefix set = {"","a","ab","abc"}
+ * suffix set = {"","a","ca","bca","abca"}
+ * 
+ * we have to find the longest proper prefix which is also a suffix
+ * 
+ * here,
+ * op = "a"
+ * 
+ */
+public class LPS {
+	
+	public static int[] findLPSArray(String str) {
+		int[] lps = new int[str.length()];
+		int i = 1; // loop starts from here
+		int n = str.length();
+		int len = 0; // stores the max value for lps[i]
+		
+		while(i<n) {
+			if (str.charAt(i) == str.charAt(len)) {
+				len++;
+				lps[i]=len;
+				i++;
+			}else {
+				if(len==0) {
+					lps[i]=0;
+					i++;
+				}else {
+					len=lps[len-1];
+				}
+			}
+		}
+		
+		return lps;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "abacabad";
+		System.out.println(Arrays.toString(findLPSArray(str)));
+		
+	}
+
+}
