2f41ef3ae8ed8ca8b73ca3d4aa8ed6e8a81bd849
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-01-30 01:40:19 +0530

    intersection of two linked lists

---
+++ Linked List/11- find intersection point of two linked list/src/LinkedList.java
@@ -0,0 +1,109 @@
+/*
+ * Find intersection of two linked lists
+ * 
+ * i/p
+ * List 1: 
+ * 10 20 30 40 
+ * List 2: 
+ * 5 11 19 25 70 80 
+ * 
+ * o/p: 30
+ * 
+ * Idea: 
+ * Get count of the nodes in the first list, let count be c1.
+ * Get a count of the nodes in the second list, let count be c2.
+ * Get the difference of counts d = abs(c1 – c2).
+ * Now traverse the bigger list from the first node till d nodes so that from here onwards both the lists have equal no of nodes.
+ * Then we can traverse both the lists in parallel till we come across a common node.
+ * 
+ * Time complexity: O(n+m)
+ * Auxiliary space: O(1)
+ */
+class Node{
+	int data;
+	Node next;
+	
+	public Node(int data) {
+		this.data = data;
+		next = null;
+	}
+}
+
+public class LinkedList {
+	
+	public static void printLinkedList(Node head) {
+		Node currNode = head;
+		while (currNode != null) {
+			System.out.print(currNode.data + " ");
+			currNode = currNode.next;
+		}
+	}
+	
+	public static int findIntersection(Node h1, Node h2) {
+		int res = -1;
+		Node curr1 = h1;
+		Node curr2 = h2;
+		int n = 0, m = 0;
+		
+		// count length of curr1
+		while (curr1 != null) {
+			n++;
+			curr1 = curr1.next;
+		}
+		
+		// count length of curr2
+		while (curr2 != null) {
+			m++;
+			curr2 = curr2.next;
+		}
+		
+		int diff = Math.abs(n-m);
+		
+		curr1 = h1;
+		curr2 = h2;
+		
+		// traversing the bigger list from the first node till "diff" nodes so that from here 
+		// onwards both the lists have equal no of nodes.
+		if (n>m) {
+			for (int i=1; i<=diff; i++)
+				curr1 = curr1.next;
+		}else {
+			for (int i=1; i<=diff; i++)
+				curr2 = curr2.next;
+		}
+		
+		// checking if there is any intersection
+		while (curr1 != null) {
+			if (curr1.data == curr2.data)
+				return curr1.data;
+			curr1 = curr1.next;
+			curr2 = curr2.next;
+		}
+		
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node head = new Node(10);
+		head.next = new Node(20);
+		head.next.next = new Node(30);
+		head.next.next.next = new Node(40);
+		
+		Node head2 = new Node(5);
+		head2.next = new Node(11);
+		head2.next.next = new Node(19);
+		head2.next.next.next = new Node(25);
+		head2.next.next.next.next = new Node(30);
+		head2.next.next.next.next.next = new Node(40);
+		
+		System.out.println("List 1: ");
+		printLinkedList(head);
+		System.out.println("\nList 2: ");
+		printLinkedList(head2);
+		
+		System.out.println("\nIntersection at node: " + findIntersection(head, head2));
+
+	}
+
+}
