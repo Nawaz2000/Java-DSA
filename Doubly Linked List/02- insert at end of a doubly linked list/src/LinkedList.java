df7f33486b93295d942b0fac9ce92597d41394a8
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-01-31 17:50:34 +0530

    insert at end of a doubly linked list

--- Doubly Linked List/02- insert at end of a doubly linked list/src/LinkedList.java
+++ Doubly Linked List/02- insert at end of a doubly linked list/src/LinkedList.java
@@ -1,7 +1,7 @@
 /*
- * Insert at beginning of a doubly linked list
+ * insert at the end of a doubly linked list
  * 
- * Time complexity: O(1)
+ * Time complexity: O(n)
  * Auxiliary space: O(1)
  */
 class Node{
@@ -16,18 +16,6 @@ class Node{
 }
 
 public class LinkedList {
-	
-	public static Node insertAtBeginning(Node head, int data) {
-		Node newNode = new Node(data);
-		if (head == null)
-			return newNode;
-		else {
-			newNode.next = head;
-			head.prev = newNode;
-			return newNode;
-		}
-	}
-	
 	public static void printLinkedList(Node head) {
 		if (head == null)
 			return;
@@ -37,12 +25,26 @@ public class LinkedList {
 			curr = curr.next;
 		}
 	}
+	
+	public static Node insertAtEnd(Node head, int data) {
+		Node newNode = new Node(data);
+		if (head == null)
+			return newNode;
+		Node curr = head;
+		while (curr.next != null)
+			curr = curr.next;
+		
+		curr.next = newNode;
+		newNode.prev = curr;
+		
+		return head;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Node head = insertAtBeginning(null, 10);
-		head = insertAtBeginning(head, 20);
-		head = insertAtBeginning(head, 30);
+		Node head = insertAtEnd(null, 10);
+		head = insertAtEnd(head, 20);
+		head = insertAtEnd(head, 30);
 		printLinkedList(head);
 
 	}
