c83c2633b7d7082bceb5b7854f03cfd1429769ce
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-02-12 17:01:40 +0530

    min heapify

---
+++ Heap/04- min heapify/src/Heap.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+/*
+ * Given an array and one node which violates the min-heap property, heapify the array
+ * 
+ * i/p: {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17}
+ * o/p: {17, 15, 13, 9, 6, 5, 10, 4, 8, 3, 1}
+ * 
+ * Idea:
+ * In each recursive loop,
+ * smallest = Min(root, left, right)
+ * if (smallest != root) then swap(smallest, root) and minHeapify(arr,smallest)
+ * 
+ * Time complexity: O(logn)
+ * Space complexity: O(logn)
+ */
+
+public class Heap {
+	public static void minHeapify(int[] arr, int i) {
+		// i is the root of tree/ sub-tree which doesn't follow the min-heap structure
+		int l = 2*i+1;
+		int r = 2*i+2;
+		
+		int smallest = i; // stores Min(root, left, right)
+		
+		// the first two if condition finds the Min(root, left, right)
+		if (l<arr.length && arr[l]<arr[i])
+			smallest = l;
+		if (r<arr.length && arr[r]<arr[smallest])
+			smallest = r;
+		
+		// if smallest == i, that means the current sub-tree is already heapified
+		// else swap and heapify
+		if (smallest != i) {
+			int temp = arr[i];
+			arr[i] = arr[smallest];
+			arr[smallest] = temp;
+			
+			minHeapify(arr, smallest);
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {40,20,30,35,25,80,32,100,70,60};
+		minHeapify(arr, 0);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
