ce70900869db88150424f6dc6feb88d361548abb
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-02-23 11:06:36 +0530

    nth fibonacci number (dynamic programming)

---
+++ Dynamic Programming/01- nth fibonacci number/src/DP.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+/*
+ * Fibonacci series using DP
+ * 
+ * Time complexity: O(n)
+ * Auxiliary space: O(n)
+ */
+public class DP {
+	// memo is used to store previous values
+	static int[] memo;
+	static int fib(int n) {
+		if (memo[n] == -1) {
+			int res = 0;
+			if (n==0 || n==1)
+				res = n;
+			else
+				res = fib(n-1) + fib(n-2);
+			memo[n] = res;
+		}
+		return memo[n];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		memo = new int[n+1];
+		Arrays.fill(memo, -1);
+		System.out.println(n + "th fibonacci number: " + fib(n));
+
+	}
+
+}
