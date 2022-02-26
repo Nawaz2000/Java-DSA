b253b8ee688a94a7222646cffeda9cd2ea60a831
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-02-26 20:49:40 +0530

    coin change count (using dp)

--- Dynamic Programming/11- coin change count problem/src/DP.java
+++ Dynamic Programming/11- coin change count problem/src/DP.java
@@ -1,15 +1,44 @@
-
+import java.util.Arrays;
+/*
+ * Given an array denoting the denomination of coins and a sum. Find out the number of
+ * combinations of currencies that add up to that sum
+ * 
+ * Ex-
+ * i/p: {1,2,3}, sum = 4
+ * o/p = 4
+ * 
+ * Explanation:
+ * 4 = 1+1+1+1
+ * 4 = 1+1+2
+ * 4 = 2+2
+ * 4 = 1+3
+ * 
+ * Idea:
+ * Everytime, 
+ * Case1: we are reducing the size of array as we are no considering the last coin
+ * Case2: we are considering the last coin
+ * 
+ * Time complexity: O(n*m)
+ * Auxiliary space: O(n*m)
+ */
 public class DP {
 	static int getCount(int[] coins, int n, int sum) {
-		if (sum == 0) 
-			return 1;
-		if (n == 0)
-			return 0;
-		int res = getCount(coins, n-1, sum);
-		if (coins[n-1] <= sum)
-			res = res + getCount(coins, n, sum-coins[n-1]);
+		int[][] memo = new int[sum+1][n+1];
+		for (int i=0; i<=n; i++)
+			memo[0][i] = 1;
+
+		for (int i=1; i<=sum; i++) {
+			for (int j=1; j<=n; j++) {
+				memo[i][j] = memo[i][j-1];
+				if (coins[j-1] <= i)
+					memo[i][j] += memo[i-coins[j-1]][j];
+			}
+		}
 		
-		return res;
+//		for (int[] x : memo)
+//			System.out.println(Arrays.toString(x));
+		
+		return memo[sum][n];
 	}
 
 	public static void main(String[] args) {
