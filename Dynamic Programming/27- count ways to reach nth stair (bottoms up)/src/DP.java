97bac6cae383c039f27c6ee9df7c648e75c3f399
Nawaz2000 <mail2nawaz2000@gmail.com>
2022-03-04 23:21:44 +0530

    count number of ways to reach the stairs (bottoms up approach)

--- Dynamic Programming/27- count ways to reach nth stair (bottoms up)/src/DP.java
+++ Dynamic Programming/27- count ways to reach nth stair (bottoms up)/src/DP.java
@@ -1,8 +1,9 @@
-import java.util.Arrays;
 /*
  * There are n stairs, a person standing at the bottom wants to reach the top. The person can 
  * climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach 
  * the top (order does matter).
+ * 
+ * Bottoms up approach
  * 
  * Input:
  * n = 4
@@ -22,28 +23,20 @@ import java.util.Arrays;
  * Auxiliary space: O(n)
  */
 public class DP {
-	static int[] memo;
 	static int countWays(int n) {
-		if (n>=0 && memo[n] != -1)
-			return memo[n];
-		if (n == 0) 
-			return 1;
+		int[] memo = new int[n+1];
+		memo[0] = 1;
+		memo[1] = 1;
 		
-		if (n < 0)
-			return 0;
+		for (int i=2; i<=n; i++)
+			memo[i] = (memo[i-1] + memo[i-2])%1000000007;
 		
-		int a = countWays(n-1);
-		int b = countWays(n-2);
-		memo[n] = (a+b)%1000000007;
 		return memo[n];
     }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 1;
-		memo = new int[n+1];
-		Arrays.fill(memo, -1);
-		memo[0] = 1;
+		int n = 10;
 		System.out.println("Number of ways: " + countWays(n));
 
 	}
